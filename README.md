85060aeb3c8bd415be0ff25132be5aea270cf4fb
LisianeAguiar <lisianeag@hotmail.com>
2019-09-18 17:41:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-3dzncv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-3dzncv)
---
+++ index.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+function changeColors () {
+
+  document.querySelectorAll('divs');
+
+}
+
+var btnEvent = document.getElementById("btn");
+btnEvent.addEventListener("click", changeColors);
