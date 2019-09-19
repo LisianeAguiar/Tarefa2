e50d8e15af13fa6ab86d88d376ee388edead06a8
LisianeAguiar <lisianeag@hotmail.com>
2019-09-19 15:38:36 -0300

    2 files modified

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ import './style.css';
 function changeColors () {
 
   var list = document.querySelectorAll(".divs");
-  
+  var newColor;
+  var currentColor;
 
+  for (var i = 0; i < list.length; i++) {
+    
+    if (i == 0) {
+
+      newColor = window.getComputedStyle(list[list.length - 1]).backgroundColor; 
+      currentColor = window.getComputedStyle(list[i]).backgroundColor;
+      list[i].style.backgroundColor = newColor;
+    }
+    else {
+
+      newColor = currentColor;
+      currentColor = window.getComputedStyle(list[i]).backgroundColor;
+      list[i].style.backgroundColor = newColor;
+    }
+  }
+ 
 }
 
 var btnEvent = document.getElementById("btn");
